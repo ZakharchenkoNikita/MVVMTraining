d2fb5d6dd9ee6615878983eb5024d4f2a51d8787
immortalprogrammist <immortalvelial@icloud.com>
2021-10-16 14:13:07 +0200

    add DetailVC

---
+++ MVVMTraining/Views/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  MVVMTraining
+//
+//  Created by Nikita on 16.10.21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
