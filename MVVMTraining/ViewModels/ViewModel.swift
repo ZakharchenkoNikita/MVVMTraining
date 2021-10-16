a86738721b55ba19ae61045b426701b20a4d7261
immortalprogrammist <immortalvelial@icloud.com>
2021-10-16 13:23:27 +0200

    setup MVVM

---
+++ MVVMTraining/ViewModels/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  MVVMTraining
+//
+//  Created by Nikita on 16.10.21.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}
---
+++ MVVMTraining/Views/ContactTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ContactTableViewCell.swift
+//  MVVMTraining
+//
+//  Created by Nikita on 16.10.21.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabale: UILabel!
+}
